274cd416f3558384687c82a33ceab7b817517ed7
Garima Chandra <garich01@noa.nintendo.com>
2015-04-15 16:59:51 -0700

    added maven compiler added view file(welcome)

---
+++ PersonalSpringMVCWebApplication/src/main/java/webapp/web/WEB-INF/jsp/welcome.js
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+        <html>
+            <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+                <head>
+                    <meta http-equiv="Content-Type" content="text/html;
+charset=ISO-8859-1">
+                        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/
+bootstrap/3.0.0/css/bootstrap.min.css">
+                            <title>Welcome</title>
+                        </head>
+                        <body>
+                            <section>
+                                <div class="jumbotron">
+                                    <div class="container">
+                                        <h1> ${greeting} </h1>
+                                        <p> ${tagline} </p>
+                                    </div>
+                                </div>
+                            </section>
+                        </body>
+                    </html>
